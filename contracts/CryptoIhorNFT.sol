e7f1a7f44d57db885d6b44b13e73d667c405cda9
Maksym Panchyshyn <maksymm.panchyshyn@gmail.com>
2022-10-16 19:50:43 +0300

    Update deploy configs

--- contracts/CryptoIhorNFT.sol
+++ contracts/CryptoIhorNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.17;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,12 +23,11 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.4',
+  solidity: '0.8.17',
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || '',
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    goerli: {
+      url: process.env.GOERLI_URL || '',
+      accounts: [process.env.PRIVATE_KEY || ''],
     },
   },
   gasReporter: {
